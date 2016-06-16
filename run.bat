58a7a1f55c4d4322670323ec9f23f1f9e3fd0718
Lifeng-Liang <lifeng.liang@gmail.com>
2016-06-16 13:38:46 +0800

    fix run.bat for any folder to run.

--- run.bat
+++ run.bat
@@ -1,12 +1,12 @@
 @echo off
 cd /d "%~dp0"
 
-haxe  -cp src -cs C:/Projects/Haxe/EightQueens/bin/ -main Main
-haxe  -cp src -cpp C:/Projects/Haxe/EightQueens/cpp/ -main Main
-haxe  -cp src -java C:/Projects/Haxe/EightQueens/java/ -main Main
-haxe  -cp src -php C:/Projects/Haxe/EightQueens/php/ -main Main
+haxe  -cp src -cs bin/ -main Main
+haxe  -cp src -cpp cpp/ -main Main
+haxe  -cp src -java java/ -main Main
+haxe  -cp src -php php/ -main Main
 
-haxe  -cp src -js C:/Projects/Haxe/EightQueens/EightQueens.js -main Main
-haxe  -cp src -python C:/Projects/Haxe/EightQueens/EightQueens.py -main Main
-haxe  -cp src -lua C:/Projects/Haxe/EightQueens/EightQueens.lua -main Main
+haxe  -cp src -js EightQueens.js -main Main
+haxe  -cp src -python EightQueens.py -main Main
+haxe  -cp src -lua EightQueens.lua -main Main
 
